61f4b965bf136bc370dd7e8d380fc369d3a40aec
Farteen <xujavacn@gmail.com>
2016-04-22 11:18:33 +0800

    Initial Commit

---
+++ PthreadTesting/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  PthreadTesting
+//
+//  Created by FarTeen on 4/22/16.
+//  Copyright © 2016 FarTeen. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
