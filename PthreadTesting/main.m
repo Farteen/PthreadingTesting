bc093f551d6854511a3f70334d2c85206033b7dd
Farteen <xujavacn@gmail.com>
2016-04-22 15:41:38 +0800

    Initial commit

--- PthreadTesting/main.m
+++ PthreadTesting/main.m
@@ -7,11 +7,44 @@
 //
 
 #import <Foundation/Foundation.h>
+#import <pthread.h>
 
-int main(int argc, const char * argv[]) {
-    @autoreleasepool {
-        // insert code here...
-        NSLog(@"Hello, World!");
-    }
+typedef struct {
+    int *ar;
+    long n;
+} subarray;
+
+
+void* incer(void *arg)
+{
+    long i;
+    
+    for (i = 0; i < ((subarray *)arg)->n; i++)
+        ((subarray *)arg)->ar[i]++;
+    printf("5555555");
+    return (void*)"123";
+}
+
+
+int main(void)
+{
+    int        ar[1000000];
+    pthread_t  th1, th2;
+    subarray   sb1, sb2;
+    
+    
+    sb1.ar = &ar[0];
+    sb1.n  = 500000;
+    (void) pthread_create(&th1, NULL, incer, &sb1);
+    
+    
+    sb2.ar = &ar[500000];
+    sb2.n  = 500000;
+    (void) pthread_create(&th2, NULL, incer, &sb2);
+    
+    
+    (void) pthread_join(th1, NULL);
+    (void) pthread_join(th2, NULL);
+    printf("......");
     return 0;
 }
